49213cc7fb5df265ee1afb51d1e832c7379f5474
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-11-21 18:46:40 +0700

    fix: update relative path

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: `/novan-ultah`,
+  pathPrefix: `/prof-dwiarso-utomo`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-eslint',
